0c8aa0d579d014b7687dbb19c8f6b0ad7f48190b
benjamin schreiber <bpschreiber2003@gmail.com>
2022-08-26 11:56:50 -0700

    hello world for cpts

---
+++ cpt_s121/week_1/aug_26/hello_world.c
@@ -0,0 +1,15 @@
+/* 
+    Name: Benjamin Schreiber
+    Date: 8/26/22
+    Class: CPTS 121
+
+    Description: hello world
+
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello World :D \n");
+    return 0;
+}
