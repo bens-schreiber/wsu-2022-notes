1d75d71cbf6ff13ffb241cc1fc1bd3d770c07def
benjamin schreiber <bpschreiber2003@gmail.com>
2022-10-14 16:53:36 -0700

    structures for yahtzee game

--- cpt_s121/week_8/yahtzee_game/game/game.c
+++ cpt_s121/week_8/yahtzee_game/game/game.c
@@ -1,4 +1,5 @@
 #include "game.h"
-void yahtzeeGame() {
+
+void startYahtzeeGame() {
     
 }
--- cpt_s121/week_8/yahtzee_game/game/game.h
+++ cpt_s121/week_8/yahtzee_game/game/game.h
@@ -1,4 +1,30 @@
 #pragma once
+#include "../dice/dice.h"
+
+typedef struct
+{
+    YahtzeeDie dice[5];
+    int playerOneScoreCard[13];
+    int playerTwoScoreCard[13];
+
+} YahtzeeGame;
+
+typedef enum
+{
+    SUM_OF_ONES,
+    SUM_OF_TWOS,
+    SUM_OF_THREES,
+    SUM_OF_FOURS,
+    SUM_OF_FIVES,
+    SUM_OF_SIXES,
+    THREE_OF_A_KIND,
+    FOUR_OF_A_KIND,
+    FULL_HOUSE,
+    SMALL_STRAIGHT,
+    LARGE_STRAIGHT,
+    YAHTZEE,
+    CHANCE
+} YahtzeeScore;
 
 // starts a game of yahtzee
-void yahtzeeGame();
+void startYahtzeeGame();
