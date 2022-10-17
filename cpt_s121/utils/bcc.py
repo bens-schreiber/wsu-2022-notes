849daba8dbc6da4550597cada9d3d20a332b756e
benjamin schreiber <bpschreiber2003@gmail.com>
2022-10-17 12:08:49 -0700

    added a helpful compile command

---
+++ cpt_s121/utils/bcc.py
@@ -0,0 +1,14 @@
+from pickle import TRUE
+import sys
+import os
+from pathlib import Path
+
+if (len(sys.argv) < 2):
+    print("file arg needed")
+    exit()
+
+c_files = " ".join([str(path.absolute()) for path in Path(sys.argv[1]).rglob("*.c")])
+
+os.system(
+    f"gcc {c_files} && ./a.out"
+)
