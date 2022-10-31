e578db1effc006330c1d16bd6ff524a1abd8d682
benjamin schreiber <bpschreiber2003@gmail.com>
2022-10-31 11:35:42 -0700

    moved battleship definitions to the battleship struct

--- cpt_s121/week_9/battleship/game/battleship_struct.h
+++ cpt_s121/week_9/battleship/game/battleship_struct.h
@@ -7,3 +7,50 @@ typedef struct
     const char graphic;
     const char *name;
 } BattleShip;
+
+
+// Different battleship possibilities
+#define BATTLE_SHIP_CARRIER(player) \
+    (BattleShip)                    \
+    {                               \
+        .hitPoints = 5,             \
+        .graphic = '#',             \
+        .name = "CARRIER",          \
+        .player = player            \
+    }
+
+#define BATTLE_SHIP_BATTLESHIP(player) \
+    (BattleShip)                       \
+    {                                  \
+        .hitPoints = 4,                \
+        .graphic = '@',                \
+        .name = "BATTLESHIP",          \
+        .player = player               \
+    }
+
+#define BATTLE_SHIP_CRUISER(player) \
+    (BattleShip)                    \
+    {                               \
+        .hitPoints = 3,             \
+        .graphic = '%',             \
+        .name = "CRUISER",          \
+        .player = player            \
+    }
+
+#define BATTLE_SHIP_SUBMARINE(player) \
+    (BattleShip)                      \
+    {                                 \
+        .hitPoints = 3,               \
+        .graphic = '$',               \
+        .name = "SUBMARINE",          \
+        .player = player              \
+    }
+
+#define BATTLE_SHIP_DESTROYER(player) \
+    (BattleShip)                      \
+    {                                 \
+        .hitPoints = 2,               \
+        .graphic = '#',               \
+        .name = "DESTROYER",          \
+        .player = player              \
+    }
