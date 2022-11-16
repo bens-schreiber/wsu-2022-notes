ddfb068267ab5c14ac2849d868a2269228be2060
benjamin schreiber <bpschreiber2003@gmail.com>
2022-11-16 09:13:22 -0800

    added poker game type

---
+++ cpt_s121/week_10/poker_game/game/poker-type.h
@@ -0,0 +1,13 @@
+#include "../utils/constants.h"
+#include "./card/card-type.h"
+
+typedef struct {
+    Card hand[POKER_CARD_AMOUNT];
+    unsigned int money;
+} PokerPlayer;
+
+typedef struct {
+    PokerPlayer player[POKER_PLAYER_AMOUNT];
+    PokerPlayer dealer;
+    Card deck[POKER_DECK_AMOUNT];
+} PokerGame;
