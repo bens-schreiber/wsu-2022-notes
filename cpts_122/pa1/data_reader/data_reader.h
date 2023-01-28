982e761a91c378219ba01cd464d8d25570435ab0
benjamin schreiber <bpschreiber2003@gmail.com>
2023-01-27 21:21:19 -0800

    moved dirs

--- cpts_122/pa1/data_reader/data_reader.h
+++
@@ -1,6 +0,0 @@
-#include "fitbit_data_t.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-FitbitResult *readAndComputeData(FILE *file);
--- cpts_122/pa1/tests/tests.h
+++ cpts_122/pa1/tests/tests.h
@@ -1,2 +1,2 @@
-#include "../data_reader/data_reader.h"
+#include "../fitbit/data_reader/data_reader.h"
 void runTests(const int runTests, FitbitResult *result);
